2e8bb023328df602212d2b2c389c53890c6de384
sabrina <sabrina@sabrinadeMacBook-Pro.local>
2019-04-11 13:04:05 +0800

    repqir display messge

--- train.py
+++ train.py
@@ -271,7 +271,7 @@ def train_normal():
             ii += 1
             num = len(data_loader) * epoch + ii
             if num % 10 == 0:
-                vis.line(X=torch.Tensor([ii]), Y=cost.data.view(-1), win='train_loss', update='append' if ii > 10 else None, opts={'title': 'train_loss'})
+                vis.line(X=torch.Tensor([num]), Y=cost.data.view(-1), win='train_loss', update='append' if num > 10 else None, opts={'title': 'train_loss'})
             
             if num % opt.displayInterval == 0:
                 
